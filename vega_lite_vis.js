f743ac0aa2e32744d4a3d9c390db889e0b3bab3d
MALB008 <110874128+MALB008@users.noreply.github.com>
2024-10-16 23:37:15 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,66 @@
+var vg_1 = "./final/choroplethwithboxes.vl.json";
+vegaEmbed("#choropleth", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_1 = "./final/Isotype.vl.json";
+vegaEmbed("#isotype", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+
+
+
+var vg_1 = "./final/donutchart.vl.json";
+vegaEmbed("#donut", vg_1).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+
+
+
+
+var vg_2 = "./final/linechart.vl.json";
+vegaEmbed("#linechart", vg_2).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+
+var vg_2 = "./final/wintergroupedbarchart.vl.json";
+vegaEmbed("#wgroupedbarchart", vg_2).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+var vg_2 = "./final/summergroupedbarchart.vl.json";
+vegaEmbed("#sgroupedbarchart", vg_2).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+
+var vg_2 = "./final/testing.vl.json";
+vegaEmbed("#testing", vg_2).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+
+
+// var vg_2 = "./final/animalproductlinechart.vl.json";
+// vegaEmbed("#animalproduct", vg_2).then(function(result) {
+                                                                           
+// }).catch(console.error);
+
+
+
+var vg_2 = "./final/testing2.vl.json";
+vegaEmbed("#testing2", vg_2).then(function(result) {
+                                                                           
+}).catch(console.error);
+
+//finalSubmitVis2\final\linecharts\animalProduct.vl.json
+
+
+
+var vg_2 = "./final/animalProduct.vl.json";
+vegaEmbed("#animalproduct", vg_2).then(function(result) {
+                                                                           
+}).catch(console.error);
