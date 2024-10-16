b48b47c9f0b851a44300d575e81b31cc6d3ce581
MALB008 <110874128+MALB008@users.noreply.github.com>
2024-10-16 23:40:16 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,9 @@
-var vg_1 = "./final/choroplethwithboxes.vl.json";
+var vg_1 = "./choroplethwithboxes.vl.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_1 = "./final/Isotype.vl.json";
+var vg_1 = "./Isotype.vl.json";
 vegaEmbed("#isotype", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
@@ -11,56 +11,48 @@ vegaEmbed("#isotype", vg_1).then(function(result) {
 
 
 
-var vg_1 = "./final/donutchart.vl.json";
+var vg_1 = "./donutchart.vl.json";
 vegaEmbed("#donut", vg_1).then(function(result) {
                                                                            
 }).catch(console.error);
 
 
 
-
-
-var vg_2 = "./final/linechart.vl.json";
-vegaEmbed("#linechart", vg_2).then(function(result) {
+// var vg_2 = "./linechart.vl.json";
+// vegaEmbed("#linechart", vg_2).then(function(result) {
                                                                            
-}).catch(console.error);
+// }).catch(console.error);
 
 
-var vg_2 = "./final/wintergroupedbarchart.vl.json";
+var vg_2 = "./wintergroupedbarchart.vl.json";
 vegaEmbed("#wgroupedbarchart", vg_2).then(function(result) {
                                                                            
 }).catch(console.error);
 
-var vg_2 = "./final/summergroupedbarchart.vl.json";
+var vg_2 = "./summergroupedbarchart.vl.json";
 vegaEmbed("#sgroupedbarchart", vg_2).then(function(result) {
                                                                            
 }).catch(console.error);
 
 
-var vg_2 = "./final/testing.vl.json";
+var vg_2 = "./testing.vl.json";
 vegaEmbed("#testing", vg_2).then(function(result) {
                                                                            
 }).catch(console.error);
 
 
 
-// var vg_2 = "./final/animalproductlinechart.vl.json";
-// vegaEmbed("#animalproduct", vg_2).then(function(result) {
-                                                                           
-// }).catch(console.error);
 
 
-
-var vg_2 = "./final/testing2.vl.json";
+var vg_2 = "./testing2.vl.json";
 vegaEmbed("#testing2", vg_2).then(function(result) {
                                                                            
 }).catch(console.error);
 
-//finalSubmitVis2\final\linecharts\animalProduct.vl.json
 
 
 
-var vg_2 = "./final/animalProduct.vl.json";
+var vg_2 = "./animalProduct.vl.json";
 vegaEmbed("#animalproduct", vg_2).then(function(result) {
                                                                            
 }).catch(console.error);
